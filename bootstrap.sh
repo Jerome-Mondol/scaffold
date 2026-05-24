de57efff8e4f29407bcae504482ba37f2e7bafde
Jerome Mondol <jeromemondol11@gmail.com>
2026-05-24 23:01:24 +0600

    feat: added simple bootstrap with basic npm installs

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,17 +6,36 @@ set -e
 # Grab the project name passed by the user
 PROJECT_NAME=$1
 
-# Validation: Block execution if they forgot to type a project name
 if [ -z "$PROJECT_NAME" ]; then
     echo "❌ Error: Please provide a project name!"
     echo "Usage: scaffold <project-name>"
     exit 1
 fi
 
-echo "========================================="
-echo "🎉 SUCCESS: The 'scaffold' global command works!"
-echo "========================================="
-echo "🚀 Target Project Name: $PROJECT_NAME"
-echo "📂 Executing from directory: $(pwd)"
-echo "⚙️  Installer mapped this script path perfectly."
-echo "========================================="
+echo "🚀 Initializing project scaffolding for: $PROJECT_NAME"
+
+# Initialize project
+mkdir "$PROJECT_NAME"
+cd "$PROJECT_NAME"
+
+# Initialize npm
+echo "📦 Initializing npm..."
+npm init -y > /dev/null
+
+
+# Take input from user for what dependecies to install
+echo "⚡ Enter the dependencies you want to install (separated by spaces):"
+echo "👉 Example: express dotenv mongoose cors"
+read -r -a DEPENDENCIES
+
+if [ ${#DEPENDENCIES[@]} -eq 0 ]; then
+    echo "ℹ️ No dependencies specified. Skipping..."
+
+else 
+    echo "📦 Installing: ${DEPENDENCIES[*]}..."
+    npm i "${DEPENDENCIES[@]}" > /dev/null
+
+    echo "Successfully installed ${DEPENDENCIES[*]}"
+fi
+
+echo "Project setup successully done!"
